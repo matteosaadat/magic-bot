34b66bfcec5019a5a3720cda22378ad5349a1f2d
Matteo <matteo.mitico@gmail.com>
2025-10-15 12:32:10 -0400

    initial setup

---
+++ src/app.py
@@ -0,0 +1,12 @@
+# app.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/")
+def hello():
+    return {"message": "hello v1"}
